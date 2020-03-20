3f5062c61a00a182832ca21ea73ca719b4c894f0
tushar8871 <tusharkhachane6@gmail.com>
2020-03-20 12:50:26 +0530

    Added : Find combination of singlet and find percentage

--- coinFlip.sh
+++ coinFlip.sh
@@ -1,12 +1,42 @@
 #!/bin/bash -x
+declare -A coinArray
+declare -A percentage
 
-echo "Welcome to coin flip simulator"
+#constants
+ONE_COIN=1;
+TWO_COIN=2;
+THREE_COIN=3
 
-randomCoinSide=$((RANDOM%2))
+#variables
+randomCoinSide=0;
+largest=0;
 
-if [ $randomCoinSide -eq 0 ]
-then
-	echo "tails"
-else
-	echo "head"
-fi 
+function coinFlip(){
+	for (( numOfFlip=1;numOfFlip<=$1;numOfFlip++ ))
+	do
+		coinSide=""
+		for (( coins=1;coins<=$2;coins++ ))
+		do
+			randomCoinSide=$((RANDOM%2))
+			if [ $randomCoinSide -eq 0 ]
+			then
+				coinSide+="T"
+			else
+				coinSide+="H"
+			fi
+				coinArray[$coinSide]=$((${coinArray[$coinSide]}+1));
+		done
+		percentage $coinSide $1
+	done
+	echo "${!percentage[@]}  : ${percentage[@]}"
+}
+
+function percentage(){
+	side=$1;
+	flip=$2;
+	percentage[$side]=$((${coinArray[$side]}*100/$flip));
+}
+
+read -p "Enter flips : " flips
+
+singletMax=$( coinFlip $flips $ONE_COIN )
